d68852edd023d19afc85365d3761618aea33df85
Jose Encarnacion <Jose.Encarnacion@dpworld.com.do>
2025-08-12 11:40:56 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Template for game vanilla javascript
+
+
+### Project Structure
+```
+template_js/
+├── index.html
+├── assets/
+│   ├── images/
+│   ├── sounds/
+│   └── fonts/
+├── css/
+│   ├── style.css
+│   
+├── libs/         <-- Third-party libraries (e.g. anim8, hump, STI)
+└── js/
+    └── script.js
+
+```
--- js/script.js
+++ js/script.js
@@ -1,3 +1,73 @@
-var canvas = /** @type {HTMLCanvasElement} */ (document.querySelector('#canvas1'));
-const ctx = canvas.getContext('2d');
+var canvas = /** @type {HTMLCanvasElement} */ (
+  document.querySelector('#canvas1')
+)
+const ctx = canvas.getContext('2d')
 
+
+
+
+// Game class
+class Game {
+  constructor () {
+    this.ctx = ctx;
+    this.isRunning = false;
+    this.lastTime = 0;
+    this.fps = 60;
+    this.frameCount = 0;
+    this.lastFpsTime = 0;
+
+
+    // settings 
+    this.start();
+  }
+
+  //update
+  update (dt) {}
+
+  //draw
+  draw () {
+
+
+    //Draw FPS
+    // this.updateFPS();
+  }
+
+  //gameLoop
+  gameLoop(currentTime){
+    if(!this.isRunning) return;
+    const deltaTime = Math.min((currentTime - this.lastTime) / 1000, 0.02)
+    this.lastTime = currentTime;
+
+    this.update(deltaTime);
+    this.draw();
+
+    requestAnimationFrame((time)=>this.gameLoop(time));
+
+  }
+  //start
+  start(){
+    this.isRunning = true;
+    this.lastTime = performance.now();
+    this.lastFpsTime = this.lastTime;
+    requestAnimationFrame((time)=>this.gameLoop(time));
+  }
+  //FPS
+  updateFPS(){
+    this.frameCount++;
+    const currentTime = performance.now();
+
+    if(currentTime - this.lastFpsTime >= 1000){
+        this.fps = this.frameCount;
+        this.frameCount = 0;
+        this.lastFpsTime = currentTime
+        console.log(`FPS: ${this.fps}`);
+    }
+  }
+
+}
+
+// Initialize the game
+let game
+window.addEventListener('load', () => {
+    game = new Game();
+})
